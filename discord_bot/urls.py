b62e20c55d5d2032965267f19040e56f3816483f
Amit <amit33mandal@gmail.com>
2022-02-19 08:36:45 +0530

    completed discord bot

--- discord_bot/urls.py
+++ discord_bot/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path,include
 from .views import HomePageView
+from discord_bot import views
 
 urlpatterns = [
     
     path('', HomePageView.as_view(),name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+
 ]
